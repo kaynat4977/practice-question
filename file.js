d822c2f49a96465f1cbe55b7104a69243506d21b
Kaynat Shabbir <shabbirkainat75@gmail.com>
2024-11-14 03:36:42 -0800

    Add files via upload

---
+++ file.js
@@ -0,0 +1,104 @@
+ //  CHAPTER TWO ASSIGMENT //
+
+//Q1: Declare a variable username?
+//let username = ("HAYA BATOOL");
+//document.write(username);
+
+//Q2: Declare a variable called myName assign to it string that reprenst your full name?
+//let myName =("My Name is Kainat shabbir");
+//document.write(myName);
+
+//Q3: write script to
+//a) Declare a js variable,titled Message?
+//b) Assign "Hello WOrld" to variable message?
+//c) Display the message in alert box?
+//let message = alert("HELLO WORLD !!");
+//document.write(message);
+
+//Q4: write a script to save student bio js variable and show in alert box?
+//let name = alert("Jhone Doe");
+//let age =alert("15 year old");
+//let course=alert("Certified Mobile Applictaion Developemnt");
+
+//Q5: write a script to display the following alert one js variable?
+ //let fastfood =alert("pizza\n pizz\n piz\n pi\n p");
+
+//Q6:DEclare a variable called email and mentioned the message in alert box?
+//let email=alert("example@example.com");
+
+//Q7:Declare a variabe book and write message "smarter way to learn javascript" in alert box?
+//let book =alert("Smarter Way To Learn Javascript");
+
+//Q8: write a script to display this in browser?
+//let message = alert("Yah!! i can write content throught javascript");
+//document.write(message);
+
+//Q9:string in variable and show in alert and browser??
+//let a =alert("$^&^");
+//document.write(a);
+
+
+//CHAPTER THREE (variable for number)//
+//Q1:
+//let age =20;
+//alert("I am" + age + "year old");
+
+//Q2:
+//let member=prompt("how many time you visit this page?");
+//alert("you have visited this page at multiple times !! " + vist +"times");
+
+//Q3:
+//let birthday =2006;
+//document.write("what is your birthday year?" + birthday + "<br>" );
+//document.write("declared variable is" + typeof (birthday));
+
+//Q4:
+//let Name =prompt("Enter your Name");
+//let product =prompt("Enter your product");
+//let quantity =prompt(" enter your quantity overhere");
+//console.log(visitorName + "order" + quantity + productName + "grocery store");
+
+
+//CHAPTER FOUR (variable name leagal and illegal)
+//Q1:
+//let name , age , email ;
+
+//Q2:
+//There are five legal variablenames;
+//let $lastName;
+//let last_Name;
+//let lastName;
+//let _LName;
+//let lastName1;
+ 
+//There are three ilegal variablenames;
+//let last_Name;
+//let *last_Name;
+//let lastName;
+
+//Q3:
+//task d 
+//document.write ("variables names should not be js keywords");
+
+//task c
+//document.write("variables names are case sensetive <br>");
+
+//task b
+//document.write("variables must begin with a letter , $ or_ example $first_name <br>");
+
+//task a
+//document.write("variable name mut conatin  , number , $_name <br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
